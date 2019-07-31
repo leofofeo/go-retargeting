c60ff87d2d61733b4b64abc497190f336447f121
leoepromos <leo.rubiano@epromos.com>
2019-07-31 16:35:52 -0500

    initial scaffolding for writing file

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"os"
+)
 
+func main() {
+	file := "7-22-19_retargeting.csv"
+	readSegmentationFile(file)
+}
+
+func readSegmentationFile(filename string) {
+	csvFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println("There seems to be a problem with your selected file. Please ensure it exists")
+		os.Exit(1)
+	}
+	reader := csv.NewReader(bufio.NewReader(csvFile))
+	fmt.Println(reader)
 }
