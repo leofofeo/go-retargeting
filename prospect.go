967b1cb242a74ef01e2c481fb5d2839624852ed0
leoepromos <leo.rubiano@epromos.com>
2019-07-31 17:11:58 -0500

    create initial prospect struct to hold row data

---
+++ prospect.go
@@ -0,0 +1,29 @@
+package main
+
+type prospect struct {
+	prospectID             int
+	firstName              string
+	lastName               string
+	email                  string
+	company                string
+	lastPageViewDate       string
+	siteID                 int
+	firstAssigned          string
+	score                  int
+	salesChannel           string
+	annualRevenue          string
+	frequency              string
+	monetary               string
+	recency                string
+	industry               string
+	seniority              string
+	employeeRange          string
+	estimatedAnnualRevenue string
+	industryGroup          string
+	sector                 string
+	segment                string
+	subIndustry            string
+	jobTitle               string
+	department             string
+	companyType            string
+}
