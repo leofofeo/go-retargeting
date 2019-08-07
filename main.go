b7fdfe13ab677475bc1c8d3f769d1e8b33c0ea2a
Leo Rubiano <leofofeo@Leos-MacBook-Pro.local>
2019-08-07 14:09:09 -0400

    remove columns and related functions from main call; whole file will most likely be deleted

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 
 func main() {
 	credentials := credentials.GetCredentials()
-	// columns := helpers.GetColumnsToKeep()
-	// columns.PrintInfo()
 	apiKey := apicalls.GetAPIKey(credentials)
 	visitorData := apicalls.GetVisitorData(apiKey, credentials["pAPIUserKey"])
 	visitorData.PrintSizeInfo()
